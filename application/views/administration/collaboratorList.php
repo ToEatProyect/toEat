4002f9815dfd856b8970706ab002ddb3f3fccf23
Jorge <jorgedaw44@gmail.com>
2017-05-16 19:25:08 +0200

    (DEV) Add "new collaborator request" to DB

---
+++ application/views/administration/collaboratorList.php
@@ -0,0 +1,47 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+
+      <?php // If exist some collaborator request ?>
+      <?php if(count($requests) > 0): ?>
+
+        <div class="table-responsive">
+          <table class="table table-striped table-hover">
+
+            <thead>
+            <tr>
+              <th>Nombre de usuario</th>
+              <th>Nombre y apellidos</th>
+              <th>Correo</th>
+              <th>Fecha creación</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <?php foreach ($requests as $request): ?>
+
+              <tr>
+                <td><?php echo $request->username ?></td>
+                <td><?php echo $request->name ?></td>
+                <td><?php echo $request->email ?></td>
+                <td><?php echo $request->created_at ?></td>
+              </tr>
+
+            <?php endforeach; ?>
+            </tbody>
+
+          </table>
+        </div>
+
+        <?php // No collaborator request? ?>
+      <?php else: ?>
+
+        <h1>En este momento no hay ninguna solicitud para nuevos colaboradores pendiente</h1>
+
+      <?php endif; ?>
+
+    </div><!-- /.col-md-12 -->
+  </div><!-- /.row -->
+</div><!-- /.container -->
